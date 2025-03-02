fed5a2302027a32b3fd0aa17769f2067d55764dd
varadsc <varadchitnis01@gmail.com>
2025-03-02 15:21:44 +0530

    Setup set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,21 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import { LoginPage } from './pages/loginPage/LoginPage'
 import { SignupPage } from './pages/signupPage/SignupPage'
+import { Layout } from './components/layout/Layout'
+import { HomePage } from './pages/homePage/HomePage'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<>Hi</>}></Route>
-      </Routes>
-      <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignupPage />}></Route>
+
+        <Route path='/' element={<Layout />}>
+            <Route path='/' element={<HomePage />} />
+        </Route>
+
       </Routes>
     </>
   )
--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import {Sidebar}  from '../../components/sidebar/Sidebar'
 import {Navbar} from '../../components/navbar/Navbar'
 
-export const PageLayout = () => {
+export const Layout = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <div style={{ display: "flex", flexDirection: "column", maxHeight: "100vh" }}>
       <Navbar />
-      <div style={{ display: "flex",height:"100%"}}>
-        <div  style={{height:"100%"}}>
-          <Sidebar/>
-        </div>
-        {/* <div className="col m-0 p-0"> */}
-          <Outlet />
-        {/* </div> */}
-      </div>
+      <Outlet />
     </div>
   )
 }
