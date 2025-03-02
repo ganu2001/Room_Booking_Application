d1be7ad5cc633b10630d642d97eefbaf0cccf329
varadsc <varadchitnis01@gmail.com>
2025-03-02 21:19:01 +0530

    Code added

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from './HomePage.module.css'
 import { getHotelsList } from '../../services/api';
+import { IoLocationOutline } from "react-icons/io5";
 
 export const HomePage = () => {
 
@@ -28,8 +29,17 @@ export const HomePage = () => {
 		<div className={styles.hotelListContainer}>
 			{hotelData?.map((hotel) => (
 				<div className={styles.hotelDataContainer}>
-					<div><img src={hotel?.hotel_images[0]} alt="image" className={styles.hotelBannerImg} /></div>
-					<div>{hotel?.name}</div>
+					<div className={styles.bannerImgContainer}><img src={hotel?.hotel_images[0]} alt="image" className={styles.hotelBannerImg} /></div>
+					<div className={styles.hotelInfoContainer}>
+						<div className={`${styles.hotelName} ${styles.infodiv}`}>{hotel?.name}</div>
+						<div className={`${styles.hotelAddress} ${styles.infodiv}`}>
+							<IoLocationOutline style={{color: 'black'}}/>
+							{hotel?.address}
+						</div>
+						<div className={`${styles.hotelCity} ${styles.infodiv}`}>{hotel?.city}</div>
+						<div className={`${styles.hotelPrice} ${styles.infodiv}`}>Prices starting from {hotel?.non_ac_room_price ? hotel?.non_ac_room_price : hotel?.ac_room_price}</div>
+						<div className={`${styles.availabilityBtn} ${styles.infodiv}`}></div>
+					</div>
 				</div>
 			))}
 		</div>
